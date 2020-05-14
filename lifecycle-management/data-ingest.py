8f21b43ce57665775d808e6ba2589a61c1c70d33
mni <mni@local>
2020-05-14 14:53:42 +0000

    updates

--- lifecycle-management/data-ingest.py
+++ lifecycle-management/data-ingest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.datasets import make_blobs
 
-df = pd.DataFrame.read_csv('argo-demo/lifecycle-management/data/data.csv')
+df = pd.read_csv('argo-demo/lifecycle-management/data/data.csv')
 X, y = make_blobs(n_samples=100, centers=3, n_features=2)
 new_df = pd.DataFrame(dict(x=X[:,0], y=X[:,1], label=y))
 df = pd.concat([df, new_df], axis=0)
